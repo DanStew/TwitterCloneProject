cef0501ffc6d168f6c4b9f9e5dee0051694038d8
DanStew <90531180+DanStew@users.noreply.github.com>
2023-08-02 21:37:10 +0100

    HTML and CSS for Login and Registration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,18 @@
 function Login(){
     return(
         <div className="Login">
-            Login
+            <div className="formContainer">
+                <div className="formWrapper">
+                    <p className="logo">Twitter Clone</p>
+                    <p className="title">Login</p>
+                    <form>
+                        <input type="email" placeholder="Email Address..." />
+                        <input type="password" placeholder="Password..." />
+                        <button>Sign In</button>
+                    </form>
+                    <p>Already have an account? <a href="">Register</a></p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,27 @@
+//Importing the needed images into this file
+import imgIcon from '../Images/imgIcon.jpg'
+
 function Register(){
     return(
         <div className="Register">
-            Register
+            <div className="formContainer">
+                <div className="formWrapper">
+                    <p className="logo">Twitter Clone</p>
+                    <p className="title">Register</p>
+                    <form>
+                        <input type="text" placeholder="Display Name..." />
+                        <input type="email" placeholder="Email Address..." />
+                        <input type="password" placeholder="Password..." />
+                        <input type="file" style={{display:"none"}} id="file"/>
+                        <label htmlFor="file">
+                            <img src={imgIcon} alt="" />
+                            <span>Add Profile Image</span>
+                        </label>
+                        <button>Sign Up</button>
+                    </form>
+                    <p>Already have an account? <a href="">Login</a></p>
+                </div>
+            </div>
         </div>
     )
 }
